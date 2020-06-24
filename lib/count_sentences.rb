203f33748b5278241957eb6e9a5ce935219a6364
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-24 18:52:29 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # binding.pry
+self.end_with?(".")
   end
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
+    self.split(/\.|\?|\!|\!!|\.../).delete_if { |word| word.length < 2}.length
 
   end
 end
