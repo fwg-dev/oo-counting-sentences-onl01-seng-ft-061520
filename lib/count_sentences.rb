17e50c0f7e3726a13612dc52d0ef3033c61e5134
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-27 19:22:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,10 @@ require 'pry'
 class String
 
   def sentence?
+<<<<<<< HEAD
     # binding.pry
+=======
+>>>>>>> 8be3f0ad507c7346d3d367705ac8604a2ecf2b53
 self.end_with?(".")
   end
 
@@ -16,7 +19,11 @@ self.end_with?("!")
   end
 
   def count_sentences
+<<<<<<< HEAD
     self.split(/\.|\?|\!|\!!|\.../).delete_if { |word| word.length < 2}.length
 
+=======
+self.count_sentences
+>>>>>>> 8be3f0ad507c7346d3d367705ac8604a2ecf2b53
   end
 end
